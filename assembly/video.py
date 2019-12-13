53cb4f2ccedc65a523efafdf5307005cc8a9526a
steve hardy <steve@marjoleinensteve.nl>
2019-12-13 23:37:33 +0100

    Fix video assembly

--- assembly/video.py
+++ assembly/video.py
@@ -11,15 +11,12 @@ class video(geometry.simple.texquad):
         
         void main()
         {
-            highp float pixsize_x = 1.0/50.0;
-            highp float pixsize_y = 1.0/10.0;
-            
             highp ivec2 coor;
             
-            coor.x = int(v_texcoor.x); // int((floor(v_texcoor.x/pixsize_x)+0.5)*pixsize_x);
-            coor.y = int(v_texcoor.y); // int((floor(v_texcoor.y/pixsize_y)+0.5)*pixsize_y);
+            coor.x = int(v_texcoor.x);
+            coor.y = int(v_texcoor.y);
 
-            f_color = texelFetch(tex, coor, 3);
+            f_color = texelFetch(tex, coor, 0);
         } """
         
     def __init__(self):
@@ -27,11 +24,8 @@ class video(geometry.simple.texquad):
         self.cap = cv2.VideoCapture(self.filename)
         self.fps = self.cap.get(cv2.CAP_PROP_FPS)
         self.frame = 0
-        print('%d fps' % self.fps)
-        
         self.w = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.h = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        
         self.tex = gl.glGenTextures(1)
         self.n = 0
         self.lastttime = 0
@@ -40,8 +34,8 @@ class video(geometry.simple.texquad):
         
 
     def getVertices(self):
-        aspect = float(self.w)/float(self.h)
-        verts = [(-aspect, +1), (+aspect, +1), (+aspect, -1), (-aspect, -1)]
+        aspect = float(self.h)/float(self.w)
+        verts = [(-1, +aspect), (+1, +aspect), (+1, -aspect), (-1, -aspect)]
         coors = [(self.w, 0), (0, 0), (0, self.h), (self.w, self.h)]
         
         return { 'position' : verts, 'texcoor' : coors }
@@ -59,7 +53,10 @@ class video(geometry.simple.texquad):
             ret, frame = self.cap.read()
                 
             gl.glBindTexture(gl.GL_TEXTURE_2D, self.tex)
-            gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, self.w, self.h, 0, gl.GL_BGR, gl.GL_UNSIGNED_BYTE, frame.tostring())
+            data = frame.tostring()
+            gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
+            gl.glTexParameterf(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
+            gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, self.w, self.h, 0, gl.GL_BGR, gl.GL_UNSIGNED_BYTE, data)
             self.frame += 1
 
         super(video, self).render()
